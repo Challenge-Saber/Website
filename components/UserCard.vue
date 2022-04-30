bd48c0fb24fef4e9c2d65beab836b77835ffe7cb
thijnmens <thijnsmulders04@gmail.com>
2022-04-30 18:20:29 +0200

    About page very gud

---
+++ components/UserCard.vue
@@ -0,0 +1,58 @@
+<script setup>
+const props = defineProps({
+	name: {
+		type: String,
+	},
+	role: {
+		type: String,
+	},
+	pic: {
+		type: String,
+	},
+});
+</script>
+
+<template>
+	<div
+		class="hover:scale-[1.15] hover:rotate-3 transition-all duration-100 hover:bg-gray-900 rounded-lg"
+	>
+		<img :src="pic" alt="ThiJNmEnS pfp" class="float-left m-3 w-24 h-24 rounded-[40px] top-1" />
+		<div class="mt-6">
+			<h3>{{ name }}</h3>
+			<h4 class="text-blue-200">{{ role }}</h4>
+			<h class="flex mr-2">
+				>
+				<a href="https://discordapp.com/users/490534335884165121"
+					><img
+						src="https://github.com/Challenge-Points/Website/blob/1b8ba0a3d98ef08394dfcb348ca9869d1bfb8ded/src/Icons/DiscordLogo.png?raw=true"
+						class="w-5 h-5 m-1"
+				/></a>
+				<a href="https://scoresaber.com/u/76561198446051555"
+					><img
+						src="https://github.com/Challenge-Points/Website/blob/1b8ba0a3d98ef08394dfcb348ca9869d1bfb8ded/src/Icons/ScoreSaberLogo.png?raw=true"
+						class="w-5 h-5 m-1"
+				/></a>
+				<a href="https://github.com/thijnmens/"
+					><img
+						src="https://github.com/Challenge-Points/Website/blob/1b8ba0a3d98ef08394dfcb348ca9869d1bfb8ded/src/Icons/GithubLogo.png?raw=true"
+						class="w-5 h-5 m-1"
+				/></a>
+				<a href="https://steamcommunity.com/id/Thijnmens/"
+					><img
+						src="https://github.com/Challenge-Points/Website/blob/1b8ba0a3d98ef08394dfcb348ca9869d1bfb8ded/src/Icons/SteamLogo.png?raw=true"
+						class="w-5 h-5 m-1"
+				/></a>
+				<a href="https://twitch.tv/thijnmens"
+					><img
+						src="https://github.com/Challenge-Points/Website/blob/1b8ba0a3d98ef08394dfcb348ca9869d1bfb8ded/src/Icons/TwitchLogo.png?raw=true"
+						class="w-5 h-5 m-1"
+				/></a>
+				<a href="https://myanimelist.net/profile/Thijnmens"
+					><img
+						src="https://github.com/Challenge-Points/Website/blob/1b8ba0a3d98ef08394dfcb348ca9869d1bfb8ded/src/Icons/MyAnimeListLogo.png?raw=true"
+						class="w-5 h-5 m-1"
+				/></a>
+			</h>
+		</div>
+	</div>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,26 @@
 <template>
 	<div>
-		<h1>About</h1>
+		<div class="grid grid-cols-4 gap-4 m-16">
+			<UserCard
+				name="Kuurama"
+				role="Project Manager"
+				pic="https://cdn.discordapp.com/avatars/324633828616568833/a_0dd84a56104f37aac0a632b9c07d6c7f.gif?size=4096"
+			/>
+			<UserCard
+				name="Thijnmens"
+				role="CS V2 Lead Dev"
+				pic="https://cdn.discordapp.com/avatars/490534335884165121/98143e642657443b8d9a84abef57dd8f.png?size=4096"
+			/>
+			<UserCard
+				name="Bmm"
+				role="Designer"
+				pic="https://cdn.discordapp.com/avatars/733473765920604261/15df9d58d402be6970cd7bf111b4ef9c.png?size=4096"
+			/>
+			<UserCard
+				name="Julien"
+				role="Bot Dev"
+				pic="https://cdn.discordapp.com/avatars/422450944652083212/3b8cb8c5425414d2467006c55c23be6b.png?size=4096"
+			/>
+		</div>
 	</div>
 </template>
